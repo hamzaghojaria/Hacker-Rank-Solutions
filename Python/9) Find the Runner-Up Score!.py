91d7dabf4af6904175f184f34569ff0b3dc4cd61
Hamza Ghojaria <34984050+hamzaghojaria@users.noreply.github.com>
2020-07-12 00:29:27 +0530

    Update and rename Find the Runner-Up Score!.py to 9) Find the Runner-Up Score!.py

--- Python/9) Find the Runner-Up Score!.py
+++ Python/9) Find the Runner-Up Score!.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split()) #To split the array in int format
     arr = list(set(list(arr)))      #To set array as a list
-    ar = len(arr) 
-    arr = sorted(arr)
-    print(arr[ar-2])
+    ar = len(arr)      # To get len arr
+    arr = sorted(arr)  #To sort the array
+    print(arr[ar-2])  #to get 2nd last digit
