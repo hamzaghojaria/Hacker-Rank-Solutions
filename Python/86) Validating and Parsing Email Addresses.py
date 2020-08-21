cbc7239586f053bbd7023191093094d3885dcb01
Hamza Ghojaria <34984050+hamzaghojaria@users.noreply.github.com>
2020-08-21 11:18:59 +0530

    Create 86) Validating and Parsing Email Addresses.py

---
+++ Python/86) Validating and Parsing Email Addresses.py
@@ -0,0 +1,9 @@
+#Solution: 
+
+import re
+n = int(input())
+for _ in range(n):
+    x, y = input().split(' ')
+    m = re.match(r'<[A-Za-z](\w|-|\.|_)+@[A-Za-z]+\.[A-Za-z]{1,3}>', y)
+    if m:
+        print(x,y)
