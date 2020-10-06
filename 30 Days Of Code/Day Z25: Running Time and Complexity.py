2b6fa0ae386ea361af6fbada2e476578bfba4497
Hamza Ghojaria <34984050+hamzaghojaria@users.noreply.github.com>
2020-10-06 12:32:34 +0530

    Create Day Z25: Running Time and Complexity.py

---
+++ 30 Days Of Code/Day Z25: Running Time and Complexity.py
@@ -0,0 +1,16 @@
+#Solution:
+
+
+n=int(input())
+for num in range(n):
+    i=int(input())
+    
+    for j in range(2,int(i**(1/2))+1):
+        if i%j==0:
+            print("Not prime")
+            break
+    else:
+        if i==1:
+            print("Not prime")
+        else:
+            print("Prime")
